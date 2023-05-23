e945656ed2d2d910116a4c09b2fc38474e4e987b
Saadraees007 <127970153+Saadraees007@users.noreply.github.com>
2023-05-24 03:32:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var inp = document.getElementById("Name");
+var inp1 = document.getElementById("SurName");
+var inp2 = document.getElementById("Number");
+var inp3 = document.getElementById("Password");
+var inp4 = document.getElementById("date");
+var inp5 = document.getElementById("month");
+var inp6 = document.getElementById("year");
+
+
+
+
+
+
+function  register(){
+    console.log(inp.value);
+    console.log(inp1.value);
+    console.log(inp2.value);
+    console.log(inp3.value);
+    console.log(inp4.value);
+    console.log(inp5.value);
+    console.log(inp6.value);
+
+}
+
